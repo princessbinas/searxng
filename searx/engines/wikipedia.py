6fdc6fcdcb344485f31f67eea827e2a3691c4f9d
Léon Tiekötter <leon@tiekoetter.com>
2022-04-27 16:47:50 +0200

    [mod] wikipedia engine: use wikiless instance
    Use wikiless instance wikiless.tiekoetter.com instead of wikipedia.org

--- searx/engines/wikipedia.py
+++ searx/engines/wikipedia.py
@@ -8,6 +8,7 @@ from json import loads
 from lxml.html import fromstring
 from searx.utils import match_language, searx_useragent
 from searx.network import raise_for_httperror
+import re
 
 # about
 about = {
@@ -78,7 +79,12 @@ def response(resp):
         return []
 
     title = api_result['title']
-    wikipedia_link = api_result['content_urls']['desktop']['page']
+    wikipedia_link_pre = api_result['content_urls']['desktop']['page']
+    wikipedia_link = (
+        re.sub('(.*\.)wikipedia\.org', 'https://wikiless.tiekoetter.com', wikipedia_link_pre)
+        + "?lang="
+        + api_result['content_urls']['desktop']['page'][8:10]
+    )
 
     results.append({'url': wikipedia_link, 'title': title})
 
